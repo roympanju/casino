679f3ba8eceac6750f3c77ea5d875560901b79df
Roy <roym@MacBook-Pro-6.local>
2018-06-08 15:04:13 -0400

    Pulling

--- src/main/java/io/zipcoder/casino/Dice.java
+++ src/main/java/io/zipcoder/casino/Dice.java
@@ -3,23 +3,7 @@ package io.zipcoder.casino;
  import java.util.Random;
 
 public class Dice {
-    int numOfDice;
 
-    public Dice(int numOfDice)
-    {
-        numOfDice = 1;
-    }
-
-    public int roll(){
-
-
-        Random rand = new Random();
-
-        int  n = rand.nextInt(numOfDice* 6) + 1;
-        return n;
-
-
-    }
-
+    return (int)(Math.Random()*6)+1);
 
 }
