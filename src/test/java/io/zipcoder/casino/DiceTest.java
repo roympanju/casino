c7c5d5efb591bb8dfbf15ad5c6ba91784ec77327
Navya Sanal <navyas@MacBook-Pro-5.local>
2018-06-08 11:16:04 -0400

    Dice changes

---
+++ src/test/java/io/zipcoder/casino/DiceTest.java
@@ -0,0 +1,18 @@
+/*package io.zipcoder.casino;
+
+import org.junit.Test;
+
+public class DiceTest {
+
+    @Test
+    public void testroll()
+    {
+        Dice die = new Dice();
+        int actual = die.roll();
+        assertTrue(rand.nextInt(numOfDice* 6) + 1);
+
+
+    }
+
+
+}
