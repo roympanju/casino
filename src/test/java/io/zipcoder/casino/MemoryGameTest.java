ac851e0a1a4790d47a49e35d8546bcc22f9d69a9
chitraB <chitrabegerhotta@yahoo.com>
2018-06-10 16:03:05 -0400

    committing test for memory game class.

--- src/test/java/io/zipcoder/casino/MemoryGameTest.java
+++ src/test/java/io/zipcoder/casino/MemoryGameTest.java
@@ -1,28 +1,34 @@
-/*package io.zipcoder.casino;
+package io.zipcoder.casino;
 
 import org.junit.Assert;
 import org.junit.Test;
 public class MemoryGameTest {
 
     @Test
-    public void test1(){
-        boolean expected = true;
+    public void testIsMatch1(){
+        boolean expected = false;
         Player[]  newPlayer = new Player[1];
-        MemoryGame mg = new MemoryGame(4,);
-        boolean actual = mg.isMatch();
-        UnitTestingUtils.assertArrayEquality(expected, actual);
+        Card c1 = new Card("spade", "4");
+        Card c2 = new Card("club", "2");
+        MemoryGame mg = new MemoryGame(4,newPlayer,4);
 
-        D
+
+        boolean actual = mg.isMatch(c1, c2);
+        Assert.assertEquals(expected, actual);
+
+
 
     }
-   /* @Test
-    public void testGuess(){
-        boolean expected = true;
-        MemoryGame memoryGame = new MemoryGame();
-        String[] actual = memoryGame.guess();
+    @Test
+    public void testIsCardListNull(){
+        boolean expected = false;
+        Player[] p1 = new Player[1];
+        MemoryGame memoryGame = new MemoryGame(4, p1, 4);
+        boolean actual = memoryGame.iscardListNull();
+        Assert.assertEquals( expected,  actual);
 
-        assertEquals( expected,  actual);
 
-    }*/
 
-}*/
+    }
+
+}
